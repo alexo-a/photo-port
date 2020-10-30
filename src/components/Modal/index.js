9938bd7192866766f812682e257230bc66014a02
Alex Ostrowski <a.ostrowski.11@gmail.com>
2020-10-30 11:43:45 -0500

    added modal test cases

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,22 +1,22 @@
 import React from 'react';
 
-function Modal({ currentPhoto }) {
-    const { name, category, description, index } = currentPhoto;
+const Modal = ({ onClose, currentPhoto }) => {
+    const { name, description, category, index } = currentPhoto;
 
     return (
         <div className="modalBackdrop">
             <div className="modalContainer">
-                <h3 className="modalTitle">{name}</h3>
+                <h3 className="modalTitle">{name} </h3>
                 <img src={require(`../../assets/large/${category}/${index}.jpg`)} alt="current category" />
                 <p>
                     {description}
                 </p>
-                <button type="button">
+                <button type="button" onClick={onClose}>
                     Close this modal
-                </button>
+        </button>
             </div>
         </div>
     );
-}
+};
 
 export default Modal;
